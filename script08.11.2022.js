8ee922f4bcfbfdd09eb78956fa5d8cf8d75db06f
Andrey <basmsk@mail.ru>
2022-11-08 13:49:53 +0500

    08.11.2022

---
+++ script08.11.2022.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// const num = 50;
+
+// if(4 == 9) {
+//     console.log('Ok!');
+// } else{
+//     console.log('Error!');
+// };
+
+// (num == 50) ? console.log('Ok!') : console.log('Error!');
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно!');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+}
